d6ac1c3d0a402dcc8011bd65d9bab753a383b57e
Tanya Chauhan <tanyachauhan084@gmail.com>
2026-02-05 15:22:45 +0530

    linked server with db

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -1,10 +1,19 @@
 import dotenv from "dotenv"
 import App from "./App.js";
+import dbConnection from "./db/db_connection.js";
 
 dotenv.config();
 
 const PORT: number = process.env.PORT ? parseInt(process.env.PORT, 10) : 8080;
 
-App.listen(PORT, ()=>{
-    console.log(`Server is listening at port:${process.env.PORT}`)
+dbConnection()
+.then(()=>{
+    App.listen(PORT, ()=>{
+        console.log(`Sever is listening at the prot:${process.env.PORT}`);
+    })
 })
+
+.catch((err: unknown)=>{
+    console.log(err);
+});
+
